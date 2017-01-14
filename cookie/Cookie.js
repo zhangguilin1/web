82992fd6724ff5c6e38502328499f5d607e3aabe
zhangguilin1 <417561581@qq.com>
2017-01-14 17:40:09 +0800

    Update Cookie.js

--- cookie/Cookie.js
+++ cookie/Cookie.js
@@ -1,1 +1,39 @@
+function removeCookie (name, path, domain) {
+    document.cookie = name + '='
+    + '; path=' + path
+    + '; domain=' + domain
+    + '; max-age=0';
+}
+
+
+function setCookie (name, value, expires, path, domain, secure) {
+    var cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+    if (expires)
+        cookie += '; expires=' + expires.toGMTString();
+    if (path)
+        cookie += '; path=' + path;
+    if (domain)
+        cookie += '; domain=' + domain;
+    if (secure)
+        cookie += '; secure=' + secure;
+    document.cookie = cookie;
+}
+
+function getcookie () {
+    var cookie = {};
+    var all = document.cookie;
+    if (all === '')
+        return cookie;
+    var list = all.split('; ');
+    for (var i = 0; i < list.length; i++) {
+        var item = list[i];
+        var p = item.indexOf('=');
+        var name = item.substring(0, p);
+        name = decodeURIComponent(name);
+        var value = item.substring(p + 1);
+        value = decodeURIComponent(value);
+        cookie[name] = value;
+    }
+    return cookie;
+}
 
